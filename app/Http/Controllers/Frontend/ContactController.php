661d1413b076dbea65fb75d75f5cbb3604f896fc
Dielleza Avdyli <da46553@ubt-uni.net>
2021-01-26 14:57:38 +0100

    no message

--- app/Http/Controllers/Frontend/ContactController.php
+++ app/Http/Controllers/Frontend/ContactController.php
@@ -9,6 +9,6 @@ class ContactController extends Controller
 {
     //
     public function index(){
-        return view('frontend.contact.contact');
+        return view('frontend.Contact.contact');
     }
 }
