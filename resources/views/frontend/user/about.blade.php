012016cf85cbd512006de1e29007a5ea0ff3b1cd
Dielleza Avdyli <da46553@ubt-uni.net>
2021-01-28 13:55:25 +0100

    no message

--- resources/views/frontend/user/about.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('frontend.layouts.app')
-@inject('content', 'App\Models\Content')
-@section('title', __('translator.issues_title'))
-
-@section('content')
-
-<h1>This is the about page .....</h1>
-
-@endsection
