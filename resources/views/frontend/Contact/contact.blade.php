66b5ad0f0e18437a9efa04cdf920d63b3a3981c8
Dielleza Avdyli <da46553@ubt-uni.net>
2021-02-11 16:51:36 +0100

    no message

--- resources/views/frontend/Contact/contact.blade.php
+++ resources/views/frontend/Contact/contact.blade.php
@@ -13,8 +13,8 @@
             <p style="text-align:center;">@lang('translator.eParticipation')</p>
           
             <h4 style="font-weight:bold;">@lang('translator.contact_address')</h4>
-            <span style="font-weight:bold;">@lang('translator.contact_address_h_1')</span><br><span style=" font-size:15px;">Kuvendi i Kosovës<br>Sheshi IBRAHIM RUGOVA nr. 5<br>Prishtinë (10000)<br></span><br>
-            <span style="font-weight:bold;"> @lang('translator.contact_address_h_2')</span><span style=" font-size:15px;"><br>Kuvendi i Kosovës<br>Rruga UÇK nr. 65<br>Prishtinë (10000)</span>
+            <span style="font-weight:bold;">@lang('translator.contact_address_h_1')</span><br><span style=" font-size:14px;">Kuvendi i Kosovës<br>Sheshi IBRAHIM RUGOVA nr. 5<br>Prishtinë (10000)<br></span><br>
+            <span style="font-weight:bold;"> @lang('translator.contact_address_h_2')</span><span style=" font-size:14px;"><br>Kuvendi i Kosovës<br>Rruga UÇK nr. 65<br>Prishtinë (10000)</span>
 
 
          <h4 style="margin-top:16px;">@lang('translator.contact_contact')</h4>   
